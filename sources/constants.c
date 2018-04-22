3d26246dc02f705278f8622d86cd707ba6ac355f
Xin Wang <dram.wang@gmail.com>
2018-04-22 14:22:30 +0800

    Rename macro to match C type

--- sources/constants.c
+++ sources/constants.c
@@ -1,13 +1,13 @@
 #include <Python.h>
 #include <stdio.h>
 
-#define INTEGER_PARAMETER "integer(c_int), parameter :: "
+#define INT_PARAMETER "integer(c_int), parameter :: "
 
 int main(void)
 {
-	printf(INTEGER_PARAMETER "py_eval_input = %d\n", Py_eval_input);
-	printf(INTEGER_PARAMETER "py_file_input = %d\n", Py_file_input);
-	printf(INTEGER_PARAMETER "py_single_input = %d\n", Py_single_input);
+	printf(INT_PARAMETER "py_eval_input = %d\n", Py_eval_input);
+	printf(INT_PARAMETER "py_file_input = %d\n", Py_file_input);
+	printf(INT_PARAMETER "py_single_input = %d\n", Py_single_input);
 
 	return 0;
 }
