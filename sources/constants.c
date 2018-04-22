2237a4621bcc051083820499dfe42ab499fc82e2
Xin Wang <dram.wang@gmail.com>
2018-04-22 14:20:27 +0800

    Constants should be declared with parameter attribute.

--- sources/constants.c
+++ sources/constants.c
@@ -1,12 +1,13 @@
 #include <Python.h>
-
 #include <stdio.h>
+
+#define INTEGER_PARAMETER "integer(c_int), parameter :: "
 
 int main(void)
 {
-	printf("integer(c_int) :: py_eval_input = %d\n", Py_eval_input);
-	printf("integer(c_int) :: py_file_input = %d\n", Py_file_input);
-	printf("integer(c_int) :: py_single_input = %d\n", Py_single_input);
+	printf(INTEGER_PARAMETER "py_eval_input = %d\n", Py_eval_input);
+	printf(INTEGER_PARAMETER "py_file_input = %d\n", Py_file_input);
+	printf(INTEGER_PARAMETER "py_single_input = %d\n", Py_single_input);
 
 	return 0;
 }
